a7d66c09347e5f715884e7469834e3a0cec7f00e
Zhangzhiyue <zhzhang@actiontec.com>
2017-07-20 22:38:36 +0800

    Bump version to 2.0.15.

--- hotShareMobile/mobile-config.js
+++ hotShareMobile/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '2.0.14',
+  version: '2.0.15',
   name: 'hotShare',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
