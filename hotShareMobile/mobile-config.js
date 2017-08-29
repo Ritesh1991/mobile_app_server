f2b16610c12ab8c572883872c9a63db5f70085f8
Zhiyue <zhiyuelive@outlook.com>
2017-08-29 17:19:18 +0800

    Bump version to 2.0.39. (#2594)

--- hotShareMobile/mobile-config.js
+++ hotShareMobile/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '2.0.38',
+  version: '2.0.39',
   name: 'hotShare',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
--- hotShareWeb/lib/6_version.js
+++ hotShareWeb/lib/6_version.js
@@ -1,1 +1,1 @@
-version_of_build = '2.0.38';
+version_of_build = '2.0.39';
