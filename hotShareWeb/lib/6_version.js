8b17e7f74f783479bbea10051595a3ce98ac5e1b
Zhiyue <zhiyuelive@outlook.com>
2017-09-12 13:05:14 +0800

    Bump version to 2.0.49. (#2672)

--- hotShareWeb/lib/6_version.js
+++ hotShareWeb/lib/6_version.js
@@ -1,1 +1,1 @@
-version_of_build = '2.0.48';
+version_of_build = '2.0.49';
--- hotShareWeb/mobile-config.js
+++ hotShareWeb/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '2.0.48',
+  version: '2.0.49',
   name: 'hotShare-web',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
