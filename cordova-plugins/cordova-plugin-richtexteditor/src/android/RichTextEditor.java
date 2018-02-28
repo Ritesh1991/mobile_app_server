86e70d430822faa5f6066b78e2eab3af4b0c9f58
Zhiyue <zhiyuelive@outlook.com>
2018-02-28 16:36:52 +0800

    更新cordova-plugins下的插件版本。 (#2980)

--- cordova-plugins/cordova-plugin-richtexteditor/src/android/RichTextEditor.java
+++ cordova-plugins/cordova-plugin-richtexteditor/src/android/RichTextEditor.java
@@ -16,7 +16,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.util.Log;
 
-import org.wordpress.android.editor.HotShareEditorActivity;
+import jp.wasabeef.richeditor.RichEditorActivity;
 
 public class RichTextEditor extends CordovaPlugin {
 	public static String TAG = "RichTextEditor";
@@ -25,17 +25,16 @@ public class RichTextEditor extends CordovaPlugin {
 	private JSONObject params;
 
 	public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
-		 this.callbackContext = callbackContext;
-		 this.params = args.getJSONObject(0);
+		this.callbackContext = callbackContext;
+		this.params = args.getJSONObject(0);
 		if (action.equals("edit")) {
-			Intent intent = new Intent(cordova.getActivity(), HotShareEditorActivity.class);
+			Intent intent = new Intent(cordova.getActivity(), RichEditorActivity.class);
 			String content = "";
 			if (this.params.has("html")) {
 				content = this.params.getString("html");
 			}
-      if (content == null)
-        content = "";
-
+            if (content == null)
+                content = "";
 			intent.putExtra("CONTENT_PARAM", content);
 			if (this.cordova != null) {
 				this.cordova.startActivityForResult((CordovaPlugin) this, intent, 0);
@@ -45,18 +44,17 @@ public class RichTextEditor extends CordovaPlugin {
 	}
 
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (resultCode == Activity.RESULT_OK && data != null) {
-      try {
-        String content = data.getStringExtra("html");
-  			JSONObject res = new JSONObject();
-        res.put("html", content);
-        res.put("text", content);
-  			this.callbackContext.success(res);
-      }
-      catch (JSONException ex) {
-        this.callbackContext.error("exception");
-      }
-
+	   if (resultCode == Activity.RESULT_OK && data != null) {
+            try {
+                String content = data.getStringExtra("html");
+          			JSONObject res = new JSONObject();
+                res.put("html", content);
+                res.put("text", content);
+          			this.callbackContext.success(res);
+            }
+            catch (JSONException ex) {
+                this.callbackContext.error("exception");
+            }
 		} else if (resultCode == Activity.RESULT_CANCELED) {
 			this.callbackContext.error("canceled");
 		} else {
