e49617e333d3fc5995c9ace39a93720429733c18
Zhiyue <zhiyuelive@outlook.com>
2018-03-21 17:32:06 +0800

    change build.sh. (#3017)

--- hotShareMobile/build.sh
+++ hotShareMobile/build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 rm -rf ~/build-hot-share
-rm -rf .meteor/local/cordova-build
-rm -rf .meteor/local/build
-rm -rf .meteor/local/bundler-cache
-rm -rf .meteor/local/plugin-cache
+rm -rf .meteor/local
 #正式版本
 meteor build ~/build-hot-share --server=https://hostgst.tiegushi.com/
 #测试版本
 #meteor build ~/build-hot-share --server=https://tsdfg.tiegushi.com/
 
-# meteor build ~/build-hot-share --server=http://storeboard.tiegushi.com
+rm -rf ~/build-hot-share/ios/project/hotShare/Images.xcassets/*
+cp -rf ../ShareExtension/Images.xcassets/* ~/build-hot-share/ios/project/hotShare/Images.xcassets/
