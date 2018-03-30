64df2d409a38ab129efdbd871e4b925692e3c146
Zhiyue <zhiyuelive@outlook.com>
2018-03-30 10:40:32 +0800

    release version 2.2.27 (#3042)

--- hotShareMobile/mobile-config.js
+++ hotShareMobile/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '2.2.26',
+  version: '2.2.27',
   name: 'hotShare',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
--- hotShareWeb/lib/6_version.js
+++ hotShareWeb/lib/6_version.js
@@ -1,1 +1,1 @@
-version_of_build = '2.2.26';
+version_of_build = '2.2.27';
