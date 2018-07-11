32968341fd751a5849d24802a39dd5817d25fe38
Roger <xning@actiontec.com>
2018-07-11 13:37:24 +0800

    Bump version to 2.2.29

--- hotShareWeb/mobile-config.js
+++ hotShareWeb/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '2.2.28',
+  version: '2.2.29',
   name: 'hotShare-web',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
