0e95102a8745d022938b0b6b7f80898eaa6757ff
Roger <xning@actiontec.com>
2018-08-08 14:14:42 +0800

    add ios auto build scripts

---
+++ hotshare-buildfiles/ios/shareEx/ShareViewController.h
@@ -0,0 +1,15 @@
+//
+//  ShareViewController.h
+//  shareEx
+//
+//  Created by aei on 5/19/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : UIViewController
+
+@end
+
