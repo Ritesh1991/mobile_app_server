5c723f16429e28995a2f608fcfcba68651ec89e0
Roger <xning@actiontec.com>
2018-08-24 16:12:33 +0800

    update jpush plugin, to solve issue of android 9.0 crash

--- hotShareMobile/mobile-config.js
+++ hotShareMobile/mobile-config.js
@@ -79,7 +79,7 @@ App.configurePlugin('org.hotshare.baidutts', {
 });
 
 App.configurePlugin('jpush-phonegap-plugin', {
-  API_KEY: '50e8f00890be941f05784e6f',
+  APP_KEY: '50e8f00890be941f05784e6f',
   CHANNEL: 'developer-default'
 });
 App.configurePlugin('phonegap-plugin-push', {
