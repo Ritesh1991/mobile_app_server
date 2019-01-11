6c75e2a192242388136c99a2a671dc7a62d3207a
Simba Zhang <solderzzc@gmail.com>
2019-01-10 17:12:07 -0800

    To save timeline data in memory based client side only collection.

--- DeepEyeMonitor/rt-portal/lib/timeline_app.js
+++ DeepEyeMonitor/rt-portal/lib/timeline_app.js
@@ -1,5 +1,29 @@
 if(Meteor.isClient){
+  ClientSideTimeLineKnown = new Meteor.Collection('client_side_timeline_known', { connection: null });
+  ClientSideTimeLineUnknown = new Meteor.Collection('client_side_timeline_unknown', { connection: null });
   Meteor.startup(function(){
+    function processProcessDeviceTimeLineDate(data){
+      if(data && data['perMin']){
+        for(min in data['perMin']){
+          if (data['perMin'].hasOwnProperty(min)) {
+            var items = data['perMin'][min]
+            if(items){
+              for(itemKey in items){
+                if (items.hasOwnProperty(itemKey)) {
+                  var person=items[itemKey]
+                  console.log(person)
+                  if(person && person.person_name){
+                    ClientSideTimeLineKnown.insert(person)
+                  } else {
+                    ClientSideTimeLineUnknown.insert(person)
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
     Devices.find().observe({
       added:function(doc){
         console.log('device added ',doc)
@@ -10,12 +34,14 @@ if(Meteor.isClient){
       added:function(id,doc){
         var group_id = DeviceTimeLine.findOne({_id:id}).group_id
         var group=SimpleChatGroups.findOne({_id:group_id})
-        console.log('timeline added ',group,' ', doc)
+        //console.log('timeline added ',group,' ', doc)
+        processProcessDeviceTimeLineDate(doc)
       },
       changed:function(id, fields){
         var group_id = DeviceTimeLine.findOne({_id:id}).group_id
         var group=SimpleChatGroups.findOne({_id:group_id})
-        console.log('timeline changed ',group,' ', fields)
+        //console.log('timeline changed ',group,' ', fields)
+        processProcessDeviceTimeLineDate(fields)
       }
     })
   })
